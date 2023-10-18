4a1f90ae2851182789d5826ecea7d009a0f9f158
githubjazon <jason.sevenants@gmail.com>
2023-10-18 14:27:53 +0000

    kjhbkbjkjb

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React, { FunctionComponent, useState } from 'react';
+import Pokemon from './models/pokemon';
+import POKEMONS from './models/mock-pokemon';
+  
+const App: FunctionComponent = () => {
+ const [pokemons] = useState<Pokemon[]>(POKEMONS);
+    
+ return (
+  <div>
+        <h1>Pokédex</h1>
+        <p>Il y a {pokemons.length} pokémons dans le pokédex.</p>
+  </div>
+ )
+}
+  
+export default App;
